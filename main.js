34f91193a4ca2d83d1b8d7f4d4b15175b8f1dc44
claytonstealth <claytonses@yahoo.com>
2022-08-04 20:21:12 -0400

    da

---
+++ main.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+console.log('Stealth is online');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('MTAwNDgxODU1NTk0NDA0MjU0Ng.G-1n_F.b6kboQBqpZV8UKri8T1KhtpEsJJ0Wz3SzGnK9s');
